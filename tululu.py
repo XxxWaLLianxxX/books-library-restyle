a1236978c321e80744799e7948438f81543a826b
XxxWaLLianxxX <vovahollivud@gmail.com>
2020-11-29 14:36:48 +0300

    Put out the creation of the arguments parser

--- tululu.py
+++ tululu.py
@@ -52,58 +52,59 @@ def pull_book_image(book_soup, page_url):
     return book_image_link, image_name
 
 
+parser = argparse.ArgumentParser(description="Программа скачивает книги с tululu.org")
+
+parser.add_argument(
+    "-sp",
+    "--start_page",
+    help="Номер первой скачиваемой страницы",
+    type=int,
+    default=1,
+)
+parser.add_argument(
+    "-ep",
+    "--end_page",
+    help="Номер последней скачиваемой страницы",
+    type=int,
+    default=1,
+)
+parser.add_argument(
+    "-si",
+    "--skip_imgs",
+    help="Не скачивать картинки",
+    action="store_true",
+)
+parser.add_argument(
+    "-st",
+    "--skip_txt",
+    help="Не скачивать книги",
+    action="store_true",
+)
+parser.add_argument(
+    "-df",
+    "--dest_folder",
+    help="Путь к каталогу с результатами парсинга",
+    default="library/",
+)
+parser.add_argument(
+    "-jp",
+    "--json_path",
+    help="Указать свой путь к *.json файлу с результатами",
+)
+
+args = parser.parse_args()
+start_page = args.start_page
+end_page = args.end_page
+dest_folder = args.dest_folder
+json_path = args.json_path
+
+os.makedirs(dest_folder, exist_ok=True)
+
+if args.json_path:
+    os.makedirs(json_path, exist_ok=True)
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Программа скачивает книги с tululu.org")
-
-    parser.add_argument(
-        "-sp",
-        "--start_page",
-        help="Номер первой скачиваемой страницы",
-        type=int,
-        default=1,
-    )
-    parser.add_argument(
-        "-ep",
-        "--end_page",
-        help="Номер последней скачиваемой страницы",
-        type=int,
-        default=1,
-    )
-    parser.add_argument(
-        "-si",
-        "--skip_imgs",
-        help="Не скачивать картинки",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-st",
-        "--skip_txt",
-        help="Не скачивать книги",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-df",
-        "--dest_folder",
-        help="Путь к каталогу с результатами парсинга",
-        default="library/",
-    )
-    parser.add_argument(
-        "-jp",
-        "--json_path",
-        help="Указать свой путь к *.json файлу с результатами",
-    )
-
-    args = parser.parse_args()
-    start_page = args.start_page
-    end_page = args.end_page
-    dest_folder = args.dest_folder
-    json_path = args.json_path
-
-    os.makedirs(dest_folder, exist_ok=True)
-
-    if args.json_path:
-        os.makedirs(json_path, exist_ok=True)
-
     books_info = []
     for page_number in range(start_page, end_page + 1):
         page_url = f'https://tululu.org/l55/{page_number}/'
